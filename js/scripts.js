34a9d965959580053488ab97884a4a7b784554f4
Student <example@example.com>
2020-01-27 16:31:15 -0800

    add a places we vizited webpage.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,67 @@
+// Front-end //
+// Global var //
+var places = new Places();
+
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $("#new-places").submit(function(event) {
     event.preventDefault();
-    // Enter needed function here //
+
+    var inputtedCountry = $("#country").val();
+    var inputtedCity = $("#city").val();
+    var inputtedMonument = $("#monument").val();
+    var newDestination = new Destination(inputtedCountry, inputtedCity, inputtedMonument);
+    places.addDestination(newDestination)
+    displayDestinationDetails(places);
+    console.log(newDestination);
+
   });
 });
+
+function displayDestinationDetails(placesToDisplay) {
+  var destinationList = $("ul#destinations");
+  var htmlForDestinationInfo = "";
+  placesToDisplay.destination.forEach(function(destination) {
+    htmlForDestinationInfo += "<li id=" + destination.id + ">" + destination.country + " " + destination.city + "</li>";
+    });
+    destinationList.html(htmlForDestinationInfo);
+};
+
+
+
+// Back-end //
+
+
+
+function Places() {
+  this.destination = [],
+  this.currentId = 0 
+}
+
+Places.prototype.addDestination = function(destination) {
+  destination.id = this.assignId();
+  this.destination.push(destination);
+}
+
+Places.prototype.assignId = function() {
+  this.currentId +=1;
+  return this.currentId;
+}
+Places.prototype.findDestinations = function(asdf) {
+  for (var i=0; i < this.destination.length; i++) {
+    if (this.destination[i]) {
+      if (this.destination[i].id == Id) {
+        return this.destination[i];
+      }
+    }
+  };
+  return false;
+}
+
+function Destination(country, city, monument) {
+  this.country = country;
+  this.city = city;
+  this.monument = monument;
+}
+Destination.prototype.visited = function() {
+  return this.country + " " + this.city + " " + this.monument;
+}
